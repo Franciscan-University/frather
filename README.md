794ee57b8ca58e891faa8a46e747fb2985637d8c
Jedediah Arnold <jarnold910@gmail.com>
2020-09-18 16:19:16 -0400

    initial game created

---
+++ README.md
@@ -0,0 +1,22 @@
+# Classic Arcade Game - Udacity p3
+
+## Getting Started & Installation
+
+To get started, clone the repository or download the zip file, then open the index.html in a browser of your choice.
+
+## Playing the Game
+The Game will begin as soon as it is opened in the browser. Each time you make it across to the end you will kill another enemy bug. When all the enemy bugs are gone, you win.
+
+To Play: Move your character using the _up_, _down_, _left_, and _right_ arrow keys. You must get your character to the other side (where the blue squares are). Each time you do, the enemy bugs will lose 1 of their companions. Once you kill all the enemy bugs, you win. 
+
+### Prerequisites
+
+No software is needed to be installed other than the source files.
+
+## Contributing
+
+Pull requests are allowed and will be reviewed on a case-by-case basis.
+
+## Authors
+
+- **Jedediah Arnold** - _Initial work_ - [Udacity](https://www.udacity.com/)
---
+++ js/app.js
@@ -0,0 +1,118 @@
+// According to engine.js x=101 and y=83 in size
+// From top: y = -23, 60, 143, 226, 309, 392
+
+// Enemies our player must avoid
+var Enemy = function(x,y,speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if (this.x < 504){
+      this.x += this.speed * dt;
+    } else {
+      this.x = -101;
+    }
+
+
+};
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Player {
+  constructor(){
+    this.sprite = "images/char-boy.png";
+    this.x = 202;
+    this.y = 370;
+  }
+  update(){
+    //how to make collision work
+    //Adapted from: https://knowledge.udacity.com/questions/10129
+    //fixed
+    for (let enemy of allEnemies) {
+      //when sharing roughly 1/2 square with enemy - hit
+      if(this.y === enemy.y && (enemy.x + 55 > this.x && enemy.x - 55 < this.x))
+      {
+        this.reset();
+      }
+    }
+    // Check for the W
+    if (this.y === 38){
+      this.winner();
+    }
+}
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  handleInput(key){
+    if (key === 'left' && this.x > 0){
+      this.x = this.x - 101;
+    } else if (key === 'right' && this.x < 404) {
+      this.x = this.x + 101;
+    } else if (key === 'up' && this.y > -23) {
+      console.log(this.y)
+      this.y = this.y - 83;
+    } else if (key === 'down' && this.y < 370) {
+      this.y = this.y + 83;
+    }
+  }
+
+  reset(){
+    this.x = 0;
+    this.y = 370;
+  }
+  winner(){
+    allEnemies.pop();
+    this.x = 202;
+    this.y = 370;
+    if (allEnemies.length === 0){
+      let youWin = document.querySelector(".winner");
+      youWin.innerHTML = "Winner!"
+    }
+  }
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+const allEnemies = [
+    new Enemy(0,121,222),
+    new Enemy(0,204,111),
+    new Enemy(0,287,15),
+    new Enemy(0,287,150),
+    new Enemy(0,204,200),
+    new Enemy(-101,121,80),
+];
+// Place the player object in a variable called player
+const player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
